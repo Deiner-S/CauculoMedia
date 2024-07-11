b4925246c9b4d5a984c54ffe7477987d5f9817c8
Deiner Rodrigues de Souza <deinersouza.tmsi@gmail.com>
2024-07-11 11:11:51 -0300

    Conclusão tarefa M8

---
+++ src/Classes/Main.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int nota1 = 8;
+		int nota2 = 5;
+		int nota3 = 6;
+		int nota4 = 9;
+		
+		int media = 0;
+		
+		media = nota1 + nota2 + nota3 + nota4;
+		media /= 4;
+		
+		System.out.println("Media da turma está em: "+ media);
+	}
+
+}
